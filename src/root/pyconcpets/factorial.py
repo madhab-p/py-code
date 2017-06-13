e9abb8f15825e96521387c6f3aa1902a399c4fa3
pneela <pneela@fhws71484.FTDCORP.NET>
2017-06-13 15:57:05 +0530

    Code is updated using "doctest" module to show how testing can be done

--- src/root/pyconcpets/factorial.py
+++ src/root/pyconcpets/factorial.py
@@ -1,12 +1,42 @@
 '''
 Created on Apr 11, 2017
+Synopsis:
+    This document exhibits how doctest module can be used
+    for testing of the module.
 
 @author: pneela
 '''
+
 def factorial(x):
+    '''
+    >>> factorial(5)
+    120
+    
+    This function should not be called with very a large number
+    >>> factorial(1329283)
+    Traceback (most recent call last):
+    ...
+    RecursionError: maximum recursion depth exceeded in comparison
+    
+    No -ve number for factorial
+    >>> factorial(-4)
+    Traceback (most recent call last):
+    ...
+    ...
+    ValueError: n must be >= 0
+    '''
+    if x < 0:
+        raise ValueError('n must be >= 0')
     if x ==1 or x == 0:
         return 1
     else:
         return x * factorial(x-1)
 
-print(factorial(5))
+
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
+
+    print(factorial(10))
